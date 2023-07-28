365203384e954478a701a98bcef69b2e09be11fe
basemaladimi <basemaladimi0@gmail.com>
2023-07-28 09:58:23 +0300

    new commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *mergeAlternately(char const *word1, char const *word2);
+
+int main() {
+    char word1[] = "abc";
+    char word2[] = "pqr";
+    char *merge = mergeAlternately(word1, word2);
+    printf("%s", merge);
+    free(merge);
+    return 0;
+}
+
+char *mergeAlternately(char const *word1, char const *word2) {
+    char const *ptr = (char *)calloc((strlen(word1) + strlen(word2) + 1), sizeof(char));
+    char *p = ptr;
+
+    while (*word1 && *word2) {
+        *(p++) = *(word1++);
+        *(p++) = *(word2++);
+    }
+
+    if (*word1) {
+        strcpy(p, word1);
+    } else if (*word2) {
+        strcpy(p, word2);
+    }
+    return ptr;
+}
